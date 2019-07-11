f681c6e28d85a069befb394f828f2db9b8cc43b1
__单调 <wangli569638598@163.com>
2019-07-11 22:19:09 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CountComp } from './components/CountCom';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+//       <CountComp num={222} onChange={(n) => {
+
+//       }}>
+
+//       </CountComp>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export class App extends React.Component {
+  state = {
+    num: 0
+  }
+  render() {
+    return (
+      <CountComp num={this.state.num} onChange={n => {
+        this.setState({
+          num: n
+        })
+      }}></CountComp>
+    )
+  }
 }
-
-export default App;
